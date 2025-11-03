589f7818fd62032cc580afb09e37c36268276b8c
ryndubei <114586905+ryndubei@users.noreply.github.com>
2025-11-03 14:53:33 +0000

    Functorial laziness in SpellT

--- spell/Spell.hs
+++ spell/Spell.hs
@@ -22,29 +22,41 @@ import Control.Monad.IO.Class
 
 -- | The Spell monad transformer, allowing interleaved side effects. For use by
 -- compiled code. Should not be exposed to the user.
-newtype SpellT m a = SpellT {
-  unSpellT :: FreeT (SpellF m) m a
-} deriving (Functor, Applicative, Monad, MonadFree (SpellF m), MonadIO)
+newtype SpellT lazy m a = SpellT {
+  unSpellT :: FreeT (SpellF lazy m) m a
+} deriving (Functor, Applicative, Monad, MonadIO)
 
-instance MonadTrans SpellT where
+-- For an unknown reason, the equivalent 'deriving newtype' fails with
+--
+-- • Couldn't match type: FreeT (SpellF lazy m) m a
+--               with: SpellT lazy m a
+--
+-- So the instance is just declared by hand.
+instance (Functor lazy, Monad m) => MonadFree (SpellF lazy m) (SpellT lazy m) where
+  {-# INLINE wrap #-}
+  wrap = wrap . coerce
+
+instance Functor lazy => MonadTrans (SpellT lazy) where
   {-# INLINE lift #-}
   lift = SpellT . lift
 
-newtype Spell a = Spell { unSpell :: FreeT (SpellF Identity) Identity a }
+newtype Spell a = Spell { unSpell :: FreeT (SpellF Identity Identity) Identity a }
   -- list of instances deliberately kept more sparse than SpellT
-  deriving (Functor, Applicative, Monad) via (SpellT Identity)
+  deriving (Functor, Applicative, Monad) via (SpellT Identity Identity)
 
-data SpellF m next
-  = Firebolt next
-  | Face (Double, Double) next
-  | forall a e. Exception e => Catch (SpellT m a) (e -> SpellT m a) (a -> next)
+data SpellF lazy m next
+  -- Anything that is to be _fully_ forced should be annotated with !,
+  -- anything else should be annotated with 'lazy'.
+  = Firebolt (lazy next)
+  | Face !Double !Double (lazy next)
+  | forall a e. Exception e => Catch (lazy (SpellT m lazy a)) (lazy (e -> SpellT m lazy a)) (lazy (a -> next))
   -- ^ NOTE: (base's) async exceptions won't be caught
-  | Throw SomeException
+  | Throw (lazy SomeException) -- marked as 'lazy' because forcing WHNF isn't necessarily sufficient
   -- ^ we can't just do throwSpell = liftF . throw because imprecise exceptions
   -- have different semantics from precise exceptions. Throwing precise
   -- exceptions has to be a dedicated side effect.
-  | PutChar Char next
-  | GetChar (Char -> next)
+  | PutChar !Char (lazy next)
+  | GetChar (lazy (Char -> next))
 
   -- (ThreadKilled should crash the level but not the whole game in case
   -- the player manages to somehow import it)
@@ -57,29 +69,29 @@ data SpellF m next
   -- | forall b. UninterruptibleMask ((forall a. Spell a -> Spell a) -> Spell b) (b -> next)
   -- -- ^ NOTE: applies only to Spell's own "async" exceptions (e.g. OutOfSideEffects)
 
-deriving instance Functor (SpellF m)
+deriving instance Functor lazy => Functor (SpellF lazy m)
 
 -- | 'liftF' for 'Spell'
-liftSpellF :: SpellF Identity a -> Spell a
-liftSpellF = coerce . (liftF @_ @(SpellT Identity))
+liftSpellF :: SpellF Identity Identity a -> Spell a
+liftSpellF = coerce . liftF @_ @(FreeT (SpellF Identity Identity) Identity)
 
 throwSpell :: Exception e => e -> Spell a
-throwSpell = liftSpellF . Throw . SomeException
+throwSpell = liftSpellF . Throw . Identity . SomeException
 
 catch :: forall e a. Exception e => Spell a -> (e -> Spell a) -> Spell a
 catch s (h :: e -> Spell a) = 
-    let s' = coerce s :: SpellT Identity a
-        h' = fmap (SpellT . unSpell) h
-     in Spell . FreeT . Identity . Free $ Catch s' h' pure
+    let s' = coerce s :: Identity (SpellT Identity Identity a)
+        h' = Identity $ fmap (SpellT . unSpell) h
+     in Spell . FreeT . Identity . Free $ Catch s' h' (Identity pure)
 
 firebolt :: Spell ()
-firebolt = liftSpellF (Firebolt ())
+firebolt = liftSpellF (Firebolt (Identity ()))
 
 face :: (Double, Double) -> Spell ()
-face v = liftSpellF (Face v ())
+face (a,b) = liftSpellF (Face a b (Identity ()))
 
 putChar :: Char -> Spell ()
-putChar c = liftSpellF (PutChar c ())
+putChar c = liftSpellF (PutChar c (Identity ()))
 
 getChar :: Spell Char
-getChar = liftSpellF (GetChar id)
+getChar = liftSpellF (GetChar (Identity id))
