0c1dab1d59c1f14d71752262e2d3fed7a7fd99b5
ryndubei <114586905+ryndubei@users.noreply.github.com>
2025-11-03 15:37:08 +0000

    Define the Untrusted monad

---
+++ src/Untrusted.hs
@@ -0,0 +1,39 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use newtype instead of data" #-}
+{-# LANGUAGE BlockArguments #-}
+module Untrusted (Untrusted, toUntrusted, withTrusted) where
+
+import Control.DeepSeq
+import Control.Exception
+import Control.Concurrent.Async
+import Control.Monad
+
+-- Deliberately not a newtype, so that it cannot be forced.
+data Untrusted a = Untrusted a deriving Functor
+
+instance Applicative Untrusted where
+  pure = toUntrusted
+  (<*>) = ap
+
+instance Monad Untrusted where
+  -- 'Untrusted ⊥' should be indistinguishable from any other Untrusted value 
+  -- in pure code.
+  -- Therefore we have to re-wrap the Untrusted after applying f, since f
+  -- may be strict.
+  (>>=) (Untrusted a) f = Untrusted (let Untrusted b = f a in b)
+
+toUntrusted :: a -> Untrusted a
+toUntrusted = Untrusted
+
+newtype SomeImpreciseException = SomeImpreciseException SomeException deriving Show
+
+instance Exception SomeImpreciseException
+
+-- | Evaluate an Untrusted value on a separate thread. The Async will either
+-- return the caught imprecise exception or a fully-forced value according to
+-- the NFData instance. Obviously, the NFData instance must be trusted.
+withTrusted :: NFData a => Untrusted a -> (Async (Either SomeException a) -> IO b) -> IO b
+withTrusted (Untrusted a) = withAsync do
+  catch
+    (fmap Right . evaluate . mapException SomeImpreciseException $ force a)
+    (\(SomeImpreciseException e) -> pure $ Left e)
