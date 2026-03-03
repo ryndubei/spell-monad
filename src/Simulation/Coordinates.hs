96d54afedfede98149c640204e0cb2a3f4fa898c
ryndubei <114586905+ryndubei@users.noreply.github.com>
2026-03-03 20:48:34 +0000

    Add plankIntegral to Simulation.Coordinates

--- src/Simulation/Coordinates.hs
+++ src/Simulation/Coordinates.hs
@@ -5,6 +5,8 @@ module Simulation.Coordinates
   , Phase
   , phaseToV
   , vToPhase
+  , LineSegment(..)
+  , plankIntegral
   , module Data.Complex
   , module Linear.Metric
   , module Linear.Quaternion
@@ -18,8 +20,8 @@ import Linear.Quaternion (Complicated(..))
 import Linear.Epsilon
 import Linear.Vector
 import Numeric.IEEE
-import Control.Exception
 import Data.Word
+import FRP.BearRiver
 
 -- | Continuous 2D coordinate type
 type V = Complex Double
@@ -35,8 +37,8 @@ pattern AmbiguousPhase :: Word64
 pattern AmbiguousPhase = 0xDEADBEEF
 
 vToPhase :: V -> Phase
-vToPhase = assert (maxNaNPayload (nan :: Double) >= AmbiguousPhase) $ \v ->
-  if nearZero v
+vToPhase v =
+  if magnitude v == 0
     -- 'phase' defaults to 0, which would likely lead to subtler bugs than a
     -- marked NaN value.
     then nanWithPayload AmbiguousPhase
@@ -46,3 +48,38 @@ vToPhase = assert (maxNaNPayload (nan :: Double) >= AmbiguousPhase) $ \v ->
 instance Metric (Complex) where
   {-# INLINE dot #-}
   dot (r1 :+ i1) (r2 :+ i2) = (r1 * r2) + (i1 * i2)
+
+data LineSegment = LineSegment { segmentEdge :: !V, segmentLine :: !V }
+
+-- | (\v -> prel v v0 v1) . (\t -> lerp t v0 v1) = id
+-- if not (nearZero (v1 - v0))
+--
+-- Measure of 'how far along' our projection is on a line segment. 
+prel :: (Fractional a, Metric f) => f a -> f a -> f a -> a
+prel v v0 v1 = ((v ^-^ v0) `dot` (v1 ^-^ v0)) / quadrance (v1 ^-^ v0)  
+
+nearestPoint :: LineSegment -> V -> V
+nearestPoint LineSegment{segmentEdge, segmentLine} v = p
+  where
+    s = prel v segmentEdge (segmentEdge + segmentLine)
+    s' = min 1 $ max 0 $ s
+    p = lerp s' segmentEdge (segmentEdge + segmentLine)
+
+-- | Clamps a 'V' to the nearest point on the line segment, then updates the
+-- position by the velocity along the segment until it exits the segment.
+-- The final value is the new position that lies outside the segment.
+--
+-- ('walking the plank')
+--
+-- Undefined on a 0-length line segment, and the closer we get to 0, the more undefined it is.
+--
+-- >>> embed (runTask (plankIntegral (LineSegment 0 1) 0)) (0, [(1, Just 0.1), (1, Just 0.1), (1, Just (-0.1)), (1, Just (-0.2))])
+-- [Left (0.0 :+ 0.0),Left (0.1 :+ 0.0),Left (0.2 :+ 0.0),Left (0.1 :+ 0.0),Right ((-0.1) :+ 0.0)]
+plankIntegral :: Monad m => LineSegment -> V -> Task Double V m V
+plankIntegral ls@LineSegment{segmentEdge, segmentLine} v = mkTask' $ proc vt -> do
+    t <- arr (t0 +) <<< integral -< vt / magnitude segmentLine
+    let v' = lerp t segmentEdge (segmentEdge + segmentLine)
+    returnA -< if 0 <= t && t <= 1 then Left v' else Right v'
+    where
+      v0 = nearestPoint ls v
+      t0 = min 1 $ max 0 $ prel v0 segmentEdge (segmentEdge + segmentLine)
